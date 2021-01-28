22ab46eb51feeca8065b0368aa2bc121a4608cd3
daniela vela <danielitavo97@gmail.com>
2021-01-28 08:31:35 -0400

    add cards

--- script.js
+++ script.js
@@ -8,10 +8,34 @@ button.addEventListener('click',(e)=>{
 })
 function getDataFromItunes(){
     let url='https://itunes.apple.com/search?term='+input.value
-    fetch(url)
+    let cors = 'https://cors-anywhere.herokuapp.com/'
+    fetch(cors+url)
     .then(data => data.json())
     .then(json => {
         console.log(json)
+
+        let finalHTML=''
+        json.results.forEach(song =>{
+            finalHTML +=
+            `
+            <div class="col s4 m4 l4">
+            <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="${song.artworkUrl100}">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">${song.trackName}<i class="material-icons right">more_vert</i></span>
+              <p>${song.artistName}</p>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+              <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
+          </div>
+          </div>
+            `
+        })
+        output.innerHTML=finalHTML
     })
     .catch(error => console.log(error))
 }
