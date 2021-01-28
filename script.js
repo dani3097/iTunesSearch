af7fc82378d704b5230417386fcc272626c3dc3e
daniela vela <danielitavo97@gmail.com>
2021-01-28 08:15:45 -0400

    add link to itunes

---
+++ script.js
@@ -0,0 +1,17 @@
+let button = document.querySelector("#submit")
+let input = document.querySelector('#search')
+let output = document.querySelector('#output')
+
+
+button.addEventListener('click',(e)=>{
+    getDataFromItunes()
+})
+function getDataFromItunes(){
+    let url='https://itunes.apple.com/search?term='+input.value
+    fetch(url)
+    .then(data => data.json())
+    .then(json => {
+        console.log(json)
+    })
+    .catch(error => console.log(error))
+}
